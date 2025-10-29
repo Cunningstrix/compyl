434e1b2084209f7f068105f0eb5d77d6d3a2c3f3
Cunningstrix <maxime.mion@gmail;com>
2025-10-29 14:53:16 +0100

    better lexer

--- lexer.py
+++ lexer.py
@@ -2,37 +2,39 @@ import re
 import sys
 
 TOKENS = {
-    'identifier' :      r'[a-zA-Z_]\w*\b',
-    'constant' :        r'[0-9]+\b',
-    'int':              r'int\b',
-    'void':             r'void\b',
-    'return':           r'return\b',
-    'open_parenthesis': r'\(',
-    'close_parenthesis':r'\)',
-    'open_brace':       r'{',
-    'close_brace':      r'}',
-    'semicolon':        r';'
+    'IDENTIFIER' :      r'[a-zA-Z_]\w*\b',
+    'COONSTANT' :        r'[0-9]+\b',
+    'INT':              r'int\b',
+    'VOID':             r'void\b',
+    'RETURN':           r'return\b',
+    'OPEN_PARENTHESIS': r'\(',
+    'CLOSE_PARENTHESIS':r'\)',
+    'OPEN_BRACE':       r'{',
+    'CLOSE_BRACE':      r'}',
+    'SEMICOLON':        r';',
+    'SKIP':             r'[ \n\t]+',
+    'MISSMATCH':        r'.'
 
 }
 
 code = sys.stdin.read()
 
-while len(code):
-    matched = False
-    #remove useless spaces. 
-    if code[0].isspace():
-        code = code[1:]
-        continue
+token_re = re.compile('|'.join(f'(?P<{name}>{pattern})' for name, pattern in TOKENS.items()))
 
-    for name,regex in TOKENS.items():
-        match = re.match(regex,code)
-        #if we have a match we process here and remvoe the match from the string.
-        if match:
-            matched = True
-            value = match.group(0)
-            print(f"TOKEN: {name} VALUE : {value}")
-            code = code[match.end():]
-            break
-    if not matched:
-        print("Error no match found at ")
-        break
+def tokenize(code):
+    tokens = []
+
+    for match in token_re.finditer(code):
+        found = match.lastgroup
+        val = match.group()
+
+        if found == 'SKIP':
+            continue
+        elif found == "MISSMATCH":
+            raise SyntaxError(f'Syntax missmatch : {val}')
+        else:
+            tokens.append((found,val))
+
+    return tokens
+
+print(tokenize(code))
